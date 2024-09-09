ade2654e0cb7bf9827fd8b3b9a3caf39d0830f0c
dantes_boom <95757720+TimpaPumpa@users.noreply.github.com>
2024-09-09 18:11:53 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,22 @@
-print("hello world")
+import threading
+import time
 
-def sum(a,b):
-    return a+b
+def task1():
+    for i in range(5):
+        print(f"Task 1 - {i}")
+        time.sleep(1)
 
-print(sum(5,3))
+def task2():
+    for i in range(5):
+        print(f"Task 2 - {i}")
+        time.sleep(1)
 
-print(sum(5,7))
+if __name__ == "__main__":
+    t1 = threading.Thread(target=task1)
+    t2 = threading.Thread(target=task2)
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
